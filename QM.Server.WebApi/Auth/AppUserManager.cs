97868a6bee00e26693c7735b3f7f1fc578239c35
gruan@asnum.com <gruan@asnum.com>
2016-04-07 11:05:28 +0800

    Use user.config to verify User

--- QM.Server.WebApi/Auth/AppUserManager.cs
+++ QM.Server.WebApi/Auth/AppUserManager.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,36 @@ namespace QM.Server.WebApi.Auth {
             : base(store) {
         }
 
-        protected override Task<bool> VerifyPasswordAsync(IUserPasswordStore<AppUser, string> store, AppUser user, string password) {
-            return base.VerifyPasswordAsync(store, user, password);
+        public override async Task<bool> CheckPasswordAsync(AppUser user, string password) {
+            var pwd = ToMD5(ToMD5(password));
+            var data = await base.CheckPasswordAsync(user, pwd);
+            return data;
+        }
+
+        protected async override Task<bool> VerifyPasswordAsync(IUserPasswordStore<AppUser, string> store, AppUser user, string password) {
+            //base.VerifyPasswordAsync 使用的是 Rfc2898DeriveBytes 密码，
+            //而这里，密码是两次MD5加密
+            //传入的 password ， 即上面的 CheckPasswordAsync 中两次MD5的结果
+            //return await base.VerifyPasswordAsync(store, user, password);
+
+            var hash = await store.GetPasswordHashAsync(user);
+            return string.Equals(hash, password);
         }
 
         internal static AppUserManager Create() {
             return new AppUserManager(new UserStore());
+        }
+
+        private static string ToMD5(string input) {
+            using (var md5Hasher = MD5.Create()) {
+                byte[] data = md5Hasher.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder sBuilder = new StringBuilder();
+
+                for (int i = 0; i < data.Length; i++) {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
         }
     }
 }
--- QM.Server.WebApi/Auth/UserStore.cs
+++ QM.Server.WebApi/Auth/UserStore.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace QM.Server.WebApi.Auth {
-    class UserStore : IUserStore<AppUser> {
+    class UserStore : IUserStore<AppUser>, IUserPasswordStore<AppUser> {
+
+        #region IUserStore
         public Task CreateAsync(AppUser user) {
             throw new NotImplementedException();
         }
@@ -34,9 +36,28 @@ namespace QM.Server.WebApi.Auth {
                 return Task.FromResult<AppUser>(null);
         }
 
+        public Task<string> GetPasswordHashAsync(AppUser user) {
+            var section = ConfigurationHelper.GetSection<UserConfig>("User");
+            if (section.User.Equals(user.UserName, StringComparison.OrdinalIgnoreCase)) {
+                return Task.FromResult(section.Pwd);
+            } else
+                return Task.FromResult<string>(null);
+        }
+
+        public Task<bool> HasPasswordAsync(AppUser user) {
+            throw new NotImplementedException();
+        }
+
+        public Task SetPasswordHashAsync(AppUser user, string passwordHash) {
+            throw new NotImplementedException();
+        }
 
         public Task UpdateAsync(AppUser user) {
             throw new NotImplementedException();
         }
+
+
+        #endregion
+
     }
 }
